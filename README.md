58641bdc767665a2715e0322ece2ee4761f94419
SamLDM <91723424+samldm@users.noreply.github.com>
2023-11-26 03:31:25 +0100

    MCGT 1.0

--- README.md
+++ README.md
@@ -1,31 +1,23 @@
-# gradient-json-minecraft
-Python script that generates a Minecraft json string with a linear gradient between two hex codes.
+# MC Gradient JSON Tool
+
+A simple tool that helps you creating json text gradient
+
+## Requirements
+
+* Tkinter
+* Numpy
 
 ## Usage
-Download the repository and open the python file inside. The file automatically closes after printing the ending string, so make sure you open it in an existing terminal.
 
-### Prompts
-![image](https://user-images.githubusercontent.com/42397332/159161930-ae0f0e09-66b2-4e0e-80db-ef0c1506f422.png)
+- Run the program
+- Fill inputs
+- Click on generate
+- Copy all the generated json
+- Use it
 
-**Text:** The string to make into a gradient.
+## Soon
 
-**Color A:** The first colour of the gradient as a hexcode. The gradient is generated left to right, so this will be the leftmost colour. The # is optional.
-
-**Color B:** The second colour of the gradient as a hexcode. The rightmost colour. The # is optional.
-
-**Bold?:** Bold text? Any input other than _true_ (case sensitive) will be considered false.
-
-**Underline?:** Underlined text? Any input other than _true_ (case sensitive) will be considered false.
-
-**Italics?:** Italic text? Any input other than _true_ (case sensitive) will be considered false.
-
-### Output
-
-The program will print a long string of text that you can then paste into any tellraw command or other command that accepts JSON.
-
-![image](https://user-images.githubusercontent.com/42397332/159162049-19eee235-cd6c-42fb-a3ce-b3650fdf505d.png)
-
-![image](https://user-images.githubusercontent.com/42397332/159162081-fbd79a8f-11bc-42c4-be9e-668ff83e4d13.png)
+- Copy to clipboard option
 
 ## Credits
-RGB to HEX and HEX to RGB functions written by [Sachin Rastogi](https://www.codespeedy.com/convert-rgb-to-hex-color-code-in-python/)
+[Original Code](https://github.com/rackodo/gradient-json-minecraft)
--- main.py
+++ main.py
@@ -1,79 +1,143 @@
 # Minecraft JSON gradient generator. If you use this script in any of your personal projects, please credit me and link the repository.
-# Script by Bash Elliott.
+# Original Script by Bash Elliott.
 
+# Fork by @SamLDM
+
+import tkinter as tk
 import numpy as np
+from tkinter import ttk
+from tkinter.colorchooser import askcolor
 
-# Hex to RGB and RGB to Hex functions by Sachin Rastogi. 
-# https://www.codespeedy.com/convert-rgb-to-hex-color-code-in-python/
 def hextorgb(hex):
     hex = hex.lstrip('#')
-    return(tuple(int(hex[i:i+2], 16) for i in (0, 2, 4)))
+    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
 def rgbtohex(rgb):
     return '%02x%02x%02x' % rgb
 
-def split(word):
-    return list(word)
+def get_readable_text_color(background_color):
+    r, g, b = hextorgb(background_color)
+    luminance = 0.299 * r / 255 + 0.587 * g / 255 + 0.114 * b / 255
+    text_color = "black" if luminance > 0.5 else "white"
+    return text_color
 
-# Initialize decorator variables.
-bold = False
-underline = False
-italics = False
 
-# Gets text and colors as hex code. Can only accept two colors.
-text = list(input("Text: "))
-ColorA = hextorgb(input("Color A: "))
-ColorB = hextorgb(input("Color B: "))
+def create_gradient(_text, _colorA, _colorB, bold=False, underline=False, italics=False):
+    text = list(_text)
+    num_points = len(text)
+    colorA = hextorgb(_colorA)
+    colorB = hextorgb(_colorB)
+    points = np.linspace(colorA, colorB, num_points, dtype=int)
+    hexes = [rgbtohex(tuple(point)) for point in points]
 
-# Sets decorator variables. If any of these return ANYTHING other than "true", it assumes they're false.
-if input("Bold?: ") == "true":
-    bold = True
-if input("Underline?: ") == "true":
-    underline = True
-if input("Italics?: ") == "true":
-    italics = True
+    final = '["",'
+    for i in range(len(hexes)):
+        do_bold = '"bold":true,' if bold else ''
+        do_underline = '"underlined":true,' if underline else ''
+        do_italics = '"italic":true,' if italics else ''
+        final += '{{"text":"{}",{}{}{}"color":"#{}"}}'.format(
+            text[i], do_bold, do_italics, do_underline, hexes[i]
+        )
+        if i != len(hexes) - 1:
+            final += ','
+    final += ']'
+    return final
 
-# Sets the number of RGB values to make in the following array.
-numPoints = len(text)
+def generate_gradient():
+    text = text_entry.get()
+    userColorA = colorA_var.get()
+    userColorB = colorB_var.get()
+    bold = bold_var.get()
+    underline = underline_var.get()
+    italics = italics_var.get()
+    print(text, userColorA, userColorB, bold, underline, italics)
 
-# Creates am array of equally spaced RGB values between ColorA and ColorB inclusive.
-points = np.linspace(ColorA, ColorB, numPoints, dtype=int)
-
-# Initializes array:hexes, then sets the array to array:points, ~hexified~.
-hexes = []
-for i in range(len(points)):
-    hexes.append((rgbtohex(tuple(points[i]))))
-
-# This mess generates the final text.
-final = '["",'
-for i in range(len(hexes)):
-    doBold = ""
-    doUnderline = ""
-    doItalics = ""
+    if text == "" or userColorA == "" or userColorB == "":
+        return
     
-    # Sets the strings to be used if the user sets their respective decorator variables to be true.
-    if bold == True:
-        doBold = '"bold":true,'
-    else:
-        doBold = ""
+    result = create_gradient(text, userColorA, userColorB, bold, underline, italics)
+    result_text.set(result)
+    print(result)
 
-    if italics == True:
-        doItalics = '"italic":true,'
-    else:
-        doItalics = ""
+def colorA_callback():
+    picker_result = askcolor(title="Choose start color")
+    colorA_var.set(("" + picker_result[1]))
+    color = picker_result[1]
+    colorA_button.configure(text=(color or "click to choose"), fg=(color and get_readable_text_color(color) or '#ffffff'), bg=(color or '#45b592'))
 
-    if underline == True:
-        doUnderline = '"underlined":true,'
-    else:
-        doUnderline = ""
+def colorB_callback():
+    picker_result = askcolor(title="Choose end color")
+    colorB_var.set(("" + picker_result[1]))
+    color = picker_result[1]
+    colorB_button.configure(text=(color or "click to choose"), fg=(color and get_readable_text_color(color) or '#ffffff'), bg=(color or '#45b592'))
 
-    # The main beast. Generates an individual json item for each character in string:text then slaps it onto the end of the final string.
-    final = final + '{{"text":"{}",{}{}{}"color":"#{}"}}'.format(text[i], doBold, doItalics, doUnderline, hexes[i])
-    # If this is the last character, don't add a comma. Otherwise, do!
-    if i != len(hexes) - 1:
-        final = final + ','
-# Finish off the json string.
-final = final + ']'
+# window
+root = tk.Tk()
+root.minsize(width=260, height=300)
+root.title("MCGT")
 
-# Print it out for the user to copy and use as fit.
-print(final)
+# top frame
+top_frame = ttk.Frame(master=root, padding=(15, 15))
+top_frame.pack(fill=tk.BOTH)
+
+title_label = ttk.Label(master=top_frame, text="MCGradientTool", font=("Arial", 18))
+title_label.pack(fill=tk.X)
+
+# input frame
+input_frame = ttk.Frame(master=root, padding=(15, 0))
+input_frame.pack(fill=tk.X)
+
+# input widgets
+text_label = ttk.Label(master=input_frame, text="Text:")
+text_label.pack(fill=tk.X)
+
+text_entry = ttk.Entry(master=input_frame)
+text_entry.pack(fill=tk.X)
+
+colorA_var = tk.StringVar()
+colorA_label = ttk.Label(master=input_frame, text="Start color:")
+colorA_label.pack(fill=tk.X)
+
+colorA_button = tk.Button(master=input_frame, command=colorA_callback, text="click to choose", fg='#ffffff', bg='#45b592', bd=0, padx=10)
+colorA_button.pack(fill=tk.X)
+
+colorB_var = tk.StringVar()
+colorB_label = ttk.Label(master=input_frame, text="End color:")
+colorB_label.pack(fill=tk.X)
+
+colorB_button = tk.Button(master=input_frame, command=colorB_callback, text="click to choose", fg='#ffffff', bg='#45b592', bd=0, padx=10)
+colorB_button.pack(fill=tk.X)
+
+
+
+#   checkbox frame
+checkbox_frame = ttk.Frame(master=input_frame, padding="10")
+checkbox_frame.pack(fill=tk.X)
+
+#   checkboxes
+bold_var = tk.BooleanVar()
+bold_checkbox = ttk.Checkbutton(master=checkbox_frame, text="Bold", variable=bold_var)
+bold_checkbox.pack(side=tk.LEFT)
+
+underline_var = tk.BooleanVar()
+underline_checkbox = ttk.Checkbutton(master=checkbox_frame, text="Underline", variable=underline_var)
+underline_checkbox.pack(side=tk.LEFT)
+
+italics_var = tk.BooleanVar()
+italics_checkbox = ttk.Checkbutton(master=checkbox_frame, text="Italics", variable=italics_var)
+italics_checkbox.pack(side=tk.LEFT)
+
+
+#   generate button
+
+generate_button = tk.Button(master=input_frame, command=generate_gradient, text="Generate", fg='#ffffff', bg='#45b592', bd=0)
+generate_button.pack(fill=tk.X)
+
+#   result text
+
+result_text = tk.StringVar()
+result_entry = ttk.Entry(master=input_frame, textvariable=result_text, state="readonly")
+result_entry.pack(fill=tk.X)
+
+
+root.mainloop()
